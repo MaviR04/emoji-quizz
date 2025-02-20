0b4da8eba23e4032bc2355e2bd362774c833e246
Mavi <mavirodrigo4@gmail.com>
2025-02-20 09:51:43 +0530

    Added hints

--- src/components/questionbox.jsx
+++ src/components/questionbox.jsx
@@ -6,16 +6,22 @@ import movies from '../movies.json'
 function QuestionBox(){
     const[index, setIndex] = useState(0)
     const[AnswerShown,setAnswer] = useState({opacity:0})
+    const[HintShown,setHint] = useState({opacity:0})
+
+    const UpdateHint =()=>{
+        setHint({opacity:1})
+    }
 
     const updateShown = ()=>{
         setAnswer({opacity:1})
     }
 
     const addIndex = ()=>{
-        if(index<=18)
+        if(index<(movies.length-1))
         {
             setIndex(index+1)
             setAnswer({opacity:0})
+            setHint({opacity:0})
         }
     }
 
@@ -23,6 +29,7 @@ function QuestionBox(){
         if(index>=1){
             setIndex(index-1)
             setAnswer({opacity:0})
+            setHint({opacity:0})
         }
         else{
             console.log("no further fiend")
@@ -35,13 +42,18 @@ function QuestionBox(){
             <div className=" text-5xl  sm:text-7xl sm:w-2xl w-full text-center bg-indigo-500 rounded-t-xl p-9">
                 {movies[index].emojis}
             </div>
-            <div className="text-4xl text-wrap sm:text-5xl sm:w-2xl w-full text-center bg-gray-50 rounded-b-xl p-9 font-bold text-black font-Inter">
+            <div className="text-4xl text-wrap sm:text-5xl sm:w-2xl w-full text-center bg-gray-50 p-9 font-bold text-black font-Inter">
                 <div style={AnswerShown}>{movies[index].name}</div>
             </div>
+            <div style={HintShown}  className=" text-5xl  sm:text-3xl sm:w-2xl w-full text-center bg-transparent rounded-b-xl p-9">
+                {movies[index].hint}
+            </div>
+            
             <div className=' flex justify-center sm:gap-20 gap-10 p-2 font-poppins mt-4 sm:flex-row flex-col '>
                 <button type="button" onClick={redIndex} className='bg-white transition-all duration-500 text-black p-2 rounded-2xl font-semibold  hover:bg-black hover:text-white'>Previous</button>
                 <button type="button" onClick={updateShown} className='bg-white transition-all duration-500 text-black p-2 rounded-2xl font-semibold  hover:bg-black hover:text-white'>Show Answer</button>
                 <button type="button" onClick={addIndex} className='bg-white transition-all duration-500 text-black p-2 rounded-2xl font-semibold hover:bg-black hover:text-white'>Next</button>
+                <button type="button" onClick={UpdateHint} className='bg-white transition-all duration-500 text-black p-2 rounded-2xl font-semibold hover:bg-black hover:text-white'>Show hint</button>
             </div>
         </div>
     )
